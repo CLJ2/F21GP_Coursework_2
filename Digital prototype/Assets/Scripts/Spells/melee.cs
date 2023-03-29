9ae004af72fcf9e50755b23ba6c09b68af3b272a
CLJ2 <c.jenkins11235@gmail.com>
2023-03-29 14:44:11 +0100

    Added melee to animator

---
+++ Digital prototype/Assets/Scripts/Spells/melee.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class melee : Spell
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void beginSpell()
+    {
+
+    }
+}
