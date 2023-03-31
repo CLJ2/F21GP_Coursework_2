0605acbaaa360e1a5120e034b71b654c06723ba6
CLJ2 <c.jenkins11235@gmail.com>
2023-03-31 16:47:43 +0100

    Wizard now casts spell which apears inworld

--- Digital prototype/Assets/Scripts/Spells/lightning.cs
+++ Digital prototype/Assets/Scripts/Spells/lightning.cs
@@ -7,9 +7,12 @@ using DigitalRuby.LightningBolt;
 public class lightning : Spell
 {
     [Header("LightningBolt")]
-    [Tooltip("A small delay to allow for casting animation")]
+    [Tooltip("A delay to allow for casting animation. This should be tweaked alongiside animation speed in animator")]
     [SerializeField]
     private float lightningBoltDelay = 0.3f;
+    [Tooltip("The length of the ability")]
+    [SerializeField]
+    private float boltDuration;
     [Tooltip("The origin point of the magic(Crystal in staff)")]
     [SerializeField]
     private Transform origin;
@@ -21,7 +24,6 @@ public class lightning : Spell
     //Other components
     private GameObject mainCamera;
     private GameObject lightningEndObj;
-    private AudioSource lightningAudio;
 
     //Awake is called when the script instance is first loaded
     private void Awake()
@@ -44,68 +46,32 @@ public class lightning : Spell
         animator = GetComponent<Animator>();
 
         //input sick custom audio
-        lightningAudio = GetComponent<AudioSource>();
-        lightningAudio.volume = 0.3f;
     }
 
     //Sets all animation parameters to ID's for faster comparison
     protected override void AssignAnimationIDs()
     {
-        animIDAbility = Animator.StringToHash("UseAbility");
+        animIDAbility = Animator.StringToHash("Charge");
     }
 
     // shoots lightning bolt
     IEnumerator throwLightningBolt()
     {
-        /**
+        gameObject.SendMessage("disableMove");
+        animator.SetBool(animIDAbility, true);
         yield return new WaitForSeconds(lightningBoltDelay);
-
-        
-        float yRot = transform.rotation.eulerAngles.y;
-        Vector3 positionAdjustment = Quaternion.Euler(0.0f, yRot, 0.0f) * Vector3.forward;
-        
-        GameObject current = GameObject.Instantiate(projectile);
-        current.GetComponent<LightningBoltScript>().StartObject = transform.gameObject;
-        
-        current.GetComponent<LightningBoltScript>().StartPosition = new Vector3(current.transform.position.x + 0.3f, current.transform.position.y + lightningBoltHeight, current.transform.position.z + 0.3f);
-
-        current.transform.position = transform.position + (positionAdjustment * lightningBoltForwardPosition);
-        //current.GetComponent<LightningBoltScript>().StartPosition = new Vector3(current.transform.position.x, current.transform.position.y + lightningBoltHeight, current.transform.position.z);
-        positionAdjustment = new Vector3(current.transform.position.x, current.transform.position.y + lightningBoltHeight, current.transform.position.z);
-        current.transform.position = positionAdjustment;
-        current.transform.rotation = mainCamera.transform.rotation;
-        //current.GetComponent<LightningBoltScript>().EndPosition = new Vector3(current.transform.position.x, current.transform.position.y + lightningBoltHeight, current.transform.position.z);
-        //current.GetComponent<LightningBoltScript>().EndPosition = new Vector3(lightningEndObj.transform.position.x, lightningEndObj.transform.position.y + lightningBoltHeight, lightningEndObj.transform.position.z);
-        current.GetComponent<LightningBoltScript>().EndObject = lightningEndObj;
-       
-       // cleaning up cloned bolts
-        Destroy(current, 0.3f);
-        */
         animator.SetBool(animIDAbility, false);
+        gameObject.SendMessage("enableMove");
+        GameObject bolt = GameObject.Instantiate(projectile);
+        GameObject boltStart = bolt.transform.GetChild(0).gameObject; //Gets first child, so dont change order of projectile prefab
+        GameObject boltEnd = bolt.transform.GetChild(1).gameObject; //Gets second child, so dont change order of projectile prefab
+        yield return new WaitForSeconds(boltDuration);
+        Destroy(bolt);
         endSpell();
-        yield return null;
-    }
-
-    // Rotates the player to face the camera direction smoothly
-    IEnumerator RotateOverTime() {
-        /**
-        float timer = 0.0f;
-        Vector3 fwd = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-        while (timer < 0.5f) {
-            timer += Time.deltaTime;
-            float t = timer / 0.5f;
-            t = t * t * t *(t *(6f *t -15f) + 10f);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(fwd), t);
-            yield return null;
-        }
-        */
-        yield return null;
     }
 
     public override void beginSpell()
     {
-        lightningAudio.Play(0);
-        animator.SetBool(animIDAbility, true);
         StartCoroutine(throwLightningBolt());
     }
 }
