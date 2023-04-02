21822968e0f4f21bce0d976185da675e2fb4a5f6
CLJ2 <c.jenkins11235@gmail.com>
2023-04-02 14:07:37 +0100

    Doors are less jank

--- Digital prototype/Assets/Scripts/door.cs
+++ Digital prototype/Assets/Scripts/door.cs
@@ -7,23 +7,37 @@ public class door : MonoBehaviour
     [Tooltip("How fasst the door opens")]
     [SerializeField]
     private int degreesPerSecond;
+    [Tooltip("Desired rotation")]
+    [SerializeField]
+    private int desiredRotation;
+
+    private float amountRotated;
+
     public bool locked;
+    private bool rotate;
     // Start is called before the first frame update
     void Start()
     {
         locked = true;
+        rotate = false;
     }
     
     public void open()
     {
         locked = false;
+        rotate = true;
     }
 
     void Update()
     {
-        if (!locked)
+        if (rotate)
         {
             transform.Rotate(new Vector3(0, degreesPerSecond, 0) * Time.deltaTime);
+            amountRotated += degreesPerSecond * Time.deltaTime;
+            if (amountRotated >= desiredRotation)
+            {
+                rotate = false;
+            }
         }
     }
 }
