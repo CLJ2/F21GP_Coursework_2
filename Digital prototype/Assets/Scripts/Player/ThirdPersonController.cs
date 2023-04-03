3dcf920b69720404e0c19a734cf77731de19200f
CLJ2 <c.jenkins11235@gmail.com>
2023-04-03 15:00:55 +0100

    Tidied up

--- Digital prototype/Assets/Scripts/Player/ThirdPersonController.cs
+++ Digital prototype/Assets/Scripts/Player/ThirdPersonController.cs
@@ -135,7 +135,6 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //SwitchCharacter();
         JumpAndGravity();
         GroundedCheck();
         Move();
@@ -154,21 +153,6 @@ public class ThirdPersonController : MonoBehaviour
         animIDGrounded = Animator.StringToHash("Grounded");
         animIDJump = Animator.StringToHash("Jump");
         animIDFreeFall = Animator.StringToHash("FreeFall");
-    }
-
-    /*
-     * Switches the character if a change happens
-     */
-    private void SwitchCharacter()
-    {
-        if (input.GetSwitchNeeded())
-        {
-            GetComponent<AiAgent>().active = true;
-            input.SetSwitchNeeded(false);
-            GetComponent<PlayerInput>().enabled = false; //Disable control of current character
-            
-            playerFollowCamera.gameObject.SendMessage("switchCharacter", input.GetCharacterSelection()); //Request control of next character
-        }
     }
 
     /*
